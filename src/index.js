a76ff42f8214b4ad7ab1ab8d7696787f139d1af7
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-03-06 14:42:55 +0200

    2nd version add content of structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/////////
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { store } from 'redux/store';
+import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/test_task_car">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
