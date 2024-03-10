424a9826b4e03a56343e46e07b824a63822619fe
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-03-10 20:28:35 +0000

    Deploying to gh-pages from @ AlexLapkin/test_task_car@5aa9751c1973ca57845e430627040902e2612dbc 🚀

--- static/js/698.bef55d69.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_car=self.webpackChunktest_task_car||[]).push([[698],{5834:function(n,t,i){i.d(t,{W:function(){return u}});var r,e=i(168),a=i(6032).Z.div(r||(r=(0,e.Z)(["\n  width: 100%;\n\n  @media (min-width: 1440px) {\n    max-width: 1184px;\n    padding-left: 128px;\n    padding-right: 128px;\n  }\n"]))),d=i(184),u=function(n){var t=n.children;return(0,d.jsx)(a,{children:t})}},9698:function(n,t,i){i.r(t);var r=i(5834),e=i(184);t.default=function(){return(0,e.jsx)(r.W,{})}}}]);
-//# sourceMappingURL=698.bef55d69.chunk.js.map
