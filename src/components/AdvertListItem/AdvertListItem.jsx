3f489c9389fc1f3f05e588cdd17c1f3b35816c94
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-03-13 21:26:42 +0200

    fixed mistakes

--- src/components/AdvertListItem/AdvertListItem.jsx
+++ src/components/AdvertListItem/AdvertListItem.jsx
@@ -4,10 +4,7 @@ import { openModal } from 'redux/modal/modalSlice';
 import {
   ItemLi,
   ItemImg,
-  ItemText,
-  ItemTextAdd,
   ItemTextCont,
-  ItemTextContAdd,
   ItemButtonLearnMore,
   FavoriteBtn,
   AddFavoriteIcon,
